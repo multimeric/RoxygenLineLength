260ec17fb63a0b7fa61ed01e2cfd939c8e4ce079
Michael Milton <ttmigueltt@gmail.com>
2023-01-09 20:38:22 +1100

    Add reproducible example

---
+++ R/doc.R
@@ -0,0 +1,12 @@
+#' Quisque porttitor mattis lorem ac commodo. Donec volutpat in mi sed porttitor
+#'
+#' @param a Maecenas et nisi vitae purus rutrum vulputate vitae a orci. Sed leo.
+#' @param b Donec sed mollis odio. Donec lacinia fermentum lectus, nec posuere.
+#' @param c Vivamus ultricies eget turpis vel molestie. Vestibulum dapibus nam.
+foo = function(a, b, c){}
+
+#' Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligu
+#'
+#' @inheritDotParams foo
+#' @export
+bar = function(...){}
